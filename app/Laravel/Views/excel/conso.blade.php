2b58bb8b5e725e7842f1182d30055477b850fd85
Joshua Arosco <joshuaarosco.hs@gmail.com>
2018-03-22 21:42:27 +0800

    Update

--- app/Laravel/Views/excel/conso.blade.php
+++ app/Laravel/Views/excel/conso.blade.php
@@ -11,7 +11,7 @@
 		<tr>
 			<td style="border: 3px solid #000; font-size: 12px;" rowspan="5"></td>
 			<td style="border: 3px solid #000; font-size: 12px;" rowspan="5"><center style="text-align: center;" >LRN</center></td>
-			<td style="border: 3px solid #000; font-size: 12px;" colspan="{{($subjects->count()*5)+21}}"><strong style="text-align: center;">GRADE SHEET</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px;" colspan="21"><strong style="text-align: center;">GRADE SHEET</strong></td>
 		</tr>
 		<tr>
 			<td style="border: 3px solid #000; font-size: 12px;"></td>
@@ -77,6 +77,58 @@
 			<td style="border: 3px solid #000; font-size: 12px;">{{$index+1}}</td>
 			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">{{$student->lrn}}</td>
 			<td style="border: 3px solid #000; font-size: 12px;">{{"{$student->lname}, {$student->fname} {$student->mname}"}}</td>
+			<?php
+				$first_grading = [];
+				$second_grading = [];
+				$third_grading = [];
+				$fourth_grading = [];
+				$average = [];
+			?>
+			@foreach($subjects as $index => $subject)
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
+				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->first_grading:0,2)}}</strong>
+			</td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
+				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->second_grading:0,2)}}</strong>
+			</td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
+				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->third_grading:0,2)}}</strong>
+			</td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
+				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->fourth_grading:0,2)}}</strong>
+			</td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
+				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->average:0,2)}}</strong>
+			</td>
+			<?php
+				array_push($first_grading,round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->first_grading:0,2));
+				array_push($second_grading,round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->second_grading:0,2));
+				array_push($third_grading,round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->third_grading:0,2));
+				array_push($fourth_grading,round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->fourth_grading:0,2));
+				array_push($average,round($subject->encode_grade($section->id,$subject->id,$student->id)?$subject->encode_grade($section->id,$subject->id,$student->id)->average:0,2));
+			?>
+			@endforeach
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($first_grading)/count($first_grading),2)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($second_grading)/count($second_grading),2)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($third_grading)/count($third_grading),2)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($fourth_grading)/count($fourth_grading),2)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($average)/count($average),2)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($first_grading)/count($first_grading),0)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($second_grading)/count($second_grading),0)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($third_grading)/count($third_grading),0)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($fourth_grading)/count($fourth_grading),0)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{round(array_sum($average)/count($average),0)}}</strong></td>
+			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong></strong></td>
 		</tr>
 		@endforeach
 	</table>
