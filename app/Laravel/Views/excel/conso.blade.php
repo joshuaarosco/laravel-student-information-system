2e6e3075121e556fa717657761d286d41301fa0b
Joshua Arosco <joshuaarosco.hs@gmail.com>
2018-03-22 21:12:24 +0800

    Update

--- app/Laravel/Views/excel/conso.blade.php
+++ app/Laravel/Views/excel/conso.blade.php
@@ -86,26 +86,26 @@
 			?>
 			@foreach($subjects as $index => $subject)
 			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
-				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)->first_grading,2)}}</strong>
+				<strong>{{$subject->encode_grade($section->id,$subject->id,$student->id)->first_grading}}</strong>
 			</td>
 			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
-				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)->second_grading,2)}}</strong>
+				<strong>{{$subject->encode_grade($section->id,$subject->id,$student->id)->second_grading}}</strong>
 			</td>
 			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
-				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)->third_grading,2)}}</strong>
+				<strong>{{$subject->encode_grade($section->id,$subject->id,$student->id)->third_grading}}</strong>
 			</td>
 			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
-				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)->fourth_grading,2)}}</strong>
+				<strong>{{$subject->encode_grade($section->id,$subject->id,$student->id)->fourth_grading}}</strong>
 			</td>
 			<td style="border: 3px solid #000; font-size: 12px; text-align: center;">
-				<strong>{{round($subject->encode_grade($section->id,$subject->id,$student->id)->average,2)}}</strong>
+				<strong>{{$subject->encode_grade($section->id,$subject->id,$student->id)->average}}</strong>
 			</td>
 			<?php
-				array_push($first_grading,round($subject->encode_grade($section->id,$subject->id,$student->id)->first_grading,2));
-				array_push($second_grading,round($subject->encode_grade($section->id,$subject->id,$student->id)->second_grading,2));
-				array_push($third_grading,round($subject->encode_grade($section->id,$subject->id,$student->id)->third_grading,2));
-				array_push($fourth_grading,round($subject->encode_grade($section->id,$subject->id,$student->id)->fourth_grading,2));
-				array_push($average,round($subject->encode_grade($section->id,$subject->id,$student->id)->average,2));
+				array_push($first_grading,$subject->encode_grade($section->id,$subject->id,$student->id)->first_grading);
+				array_push($second_grading,$subject->encode_grade($section->id,$subject->id,$student->id)->second_grading);
+				array_push($third_grading,$subject->encode_grade($section->id,$subject->id,$student->id)->third_grading);
+				array_push($fourth_grading,$subject->encode_grade($section->id,$subject->id,$student->id)->fourth_grading);
+				array_push($average,$subject->encode_grade($section->id,$subject->id,$student->id)->average);
 			?>
 			@endforeach
 			<td style="border: 3px solid #000; font-size: 12px; text-align: center;"><strong>{{array_sum($first_grading)/count($first_grading)}}</strong></td>
