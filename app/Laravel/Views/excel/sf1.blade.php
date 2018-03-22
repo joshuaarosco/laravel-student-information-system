be2e0ad923214474cd0ab88abf7c37d8b66f4b87
Joshua Arosco <joshuaarosco.hs@gmail.com>
2018-03-22 21:03:16 +0800

    Update

--- app/Laravel/Views/excel/sf1.blade.php
+++ app/Laravel/Views/excel/sf1.blade.php
@@ -5,19 +5,19 @@
 <body>
 	<table>
 		<tr>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;"></td>
-			<td rowspan="2" style="border: 2px solid #000;  text-align: center;">LRN</td>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;">NAME <br>(Last Name, First Name, Middle Name)</td>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;">Sex <br>(M/F)</td>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;">BIRTHDATE <br> (mm/dd/yyyy)</td>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;">AGE as of 1st Friday June</td>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;">MOTHER TONGUE <br>(Grade 1 to 3 Only)</td>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;">IP <br>(Ethnic Group)</td>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;">Religion</td>
-			<td colspan="4" style="border: 2px solid #000; text-align: center;">Address</td>
-			<td colspan="2" style="border: 2px solid #000;  text-align: center;">Parents</td>
-			<td colspan="2" style="border: 2px solid #000; text-align: center;">Guardian <br>(if Not Parent)</td>
-			<td rowspan="2" style="border: 2px solid #000; text-align: center;">Contact Number of Parent or Guardian</td>
+			<td rowspan="2" width="5%"  style="border: 2px solid #000; text-align: center;"></td>
+			<td rowspan="2" width="15%" style="border: 2px solid #000;  text-align: center;">LRN</td>
+			<td rowspan="2" width="50%" style="border: 2px solid #000; text-align: center;">NAME <br>(Last Name, First Name, Middle Name)</td>
+			<td rowspan="2" width="20%" style="border: 2px solid #000; text-align: center;">Sex <br>(M/F)</td>
+			<td rowspan="2" width="40%" style="border: 2px solid #000; text-align: center;">BIRTHDATE <br> (mm/dd/yyyy)</td>
+			<td rowspan="2" width="40%" style="border: 2px solid #000; text-align: center;">AGE as of 1st Friday June</td>
+			<td rowspan="2" width="40%" style="border: 2px solid #000; text-align: center;">MOTHER TONGUE <br>(Grade 1 to 3 Only)</td>
+			<td rowspan="2" width="20%" style="border: 2px solid #000; text-align: center;">IP <br>(Ethnic Group)</td>
+			<td rowspan="2" width="20%" style="border: 2px solid #000; text-align: center;">Religion</td>
+			<td colspan="4" width="20%" style="border: 2px solid #000; text-align: center;">Address</td>
+			<td colspan="2" width="100%" style="border: 2px solid #000;  text-align: center;">Parents</td>
+			<td colspan="2" width="20%" style="border: 2px solid #000; text-align: center;">Guardian <br>(if Not Parent)</td>
+			<td rowspan="2" width="40%" style="border: 2px solid #000; text-align: center;">Contact Number of Parent or Guardian</td>
 			<td style="text-align: center;">Remarks</td>
 		</tr>
 		<tr>
@@ -30,16 +30,16 @@
 			<td></td>
 			<td></td>
 			<td></td>
-			<td style="border: 2px solid #000; text-align: center;">House #/ Street/ Sitio/ Purok</td>
-			<td style="border: 2px solid #000; text-align: center;">Barangay</td>
-			<td style="border: 2px solid #000; text-align: center;">Municipality/ City</td>
-			<td style="border: 2px solid #000; text-align: center;">Province</td>
-			<td style="border: 2px solid #000; text-align: center;">Father's Name (Last Name, First Name, Middle Name)</td>
-			<td style="border: 2px solid #000; text-align: center;">Mother's Maiden Name (Last Name, First Name, Middle </td>
-			<td style="border: 2px solid #000; text-align: center;">Name</td>
-			<td style="border: 2px solid #000; text-align: center;">Relationship</td>
+			<td width="20%" style="border: 2px solid #000; text-align: center;">House #/ Street/ Sitio/ Purok</td>
+			<td width="20%" style="border: 2px solid #000; text-align: center;">Barangay</td>
+			<td width="20%" style="border: 2px solid #000; text-align: center;">Municipality/ City</td>
+			<td width="20%" style="border: 2px solid #000; text-align: center;">Province</td>
+			<td width="50%" style="border: 2px solid #000; text-align: center;">Father's Name (Last Name, First Name, Middle Name)</td>
+			<td width="50%" style="border: 2px solid #000; text-align: center;">Mother's Maiden Name (Last Name, First Name, Middle </td>
+			<td width="20%" style="border: 2px solid #000; text-align: center;">Name</td>
+			<td width="20%" style="border: 2px solid #000; text-align: center;">Relationship</td>
 			<td></td>
-			<td style="border: 2px solid #000; text-align: center;">(Please refer to the legend on last page)</td>
+			<td width="40%" style="border: 2px solid #000; text-align: center;">(Please refer to the legend on last page)</td>
 		</tr>
 		@foreach($students as $index => $student)
 		<tr>
